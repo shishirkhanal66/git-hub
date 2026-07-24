b8f0107f5780f20136bf18a98bd577237e0717b4
shishirkhanal66 <shishir.khanal.153@gmail.com>
2026-07-24 20:15:38 +0545

    added more feature

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ removeButtons.forEach(button =>{
 
     });
 });
+function addToCart(name,price,image){
+    let cart=localStorage.getItem("cart");
+    if(cart){
+        cart=JSON.parse(cart);
+    }
+    else{
+        cart=[];
+    }
+    let item={
+        name:name,
+        price:price,
+        image:image
+    };
+    cart.push(item);
+    localStorage.setItem("cart",JSON.stringify(cart));
+    alert("Product added to cart!");
+}

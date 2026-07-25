e42f3ca29fd8ec780830b630210ad51e4488e40a
shishirkhanal66 <shishir.khanal.153@gmail.com>
2026-07-25 16:20:41 +0545

    new changes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const removeButtons= document.querySelectorAll(".next");
-removeButtons.forEach(button =>{
-    button.addEventListener("click", function(e){
-        e.preventDefault();
-        this.closest(".shishir").remove();
-
-    });
-});
+function removeItem(index){
+    let cart=JSON.parse(localStorage.getItem("cart")) || [];
+    cart.splice(index,1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    location.reload();
+}
    
 function openProduct(image,name,price){
     let product={
@@ -19,8 +17,50 @@ function openProduct(image,name,price){
 function addToCart(){
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
     let product=JSON.parse(localStorage.getItem("product"));
+    if(product){
     cart.push(product);
     localStorage.setItem("cart", JSON.stringify(cart));
     alert("Product added to cart!");
     window.location.href="cart.html";
+    }
 }
+window.onload = function () {
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    let cartBody = document.getElementById("cart-body");
+
+    if (!cartBody) {
+        return;
+    }
+
+    cartBody.innerHTML = "";
+
+    for (let i = 0; i < cart.length; i++) {
+
+        cartBody.innerHTML += `
+        <tr class="shishir">
+            <td>
+                <a href="#" onclick="removeItem(${i}); return false;">
+                    <i class="far fa-times-circle"></i>
+                </a>
+            </td>
+
+            <td>
+                <img src="${cart[i].image}" width="80">
+            </td>
+
+            <td>${cart[i].name}</td>
+
+            <td>$${cart[i].price}</td>
+
+            <td>
+                <input type="number" value="1" min="1">
+            </td>
+
+            <td>$${cart[i].price}</td>
+        </tr>
+        `;
+    }
+
+};

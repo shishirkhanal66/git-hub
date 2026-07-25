a5f36092892dbcaf74f5577675168e013154aad9
shishirkhanal66 <shishir.khanal.153@gmail.com>
2026-07-25 18:54:42 +0545

    added price feature

--- script.js
+++ script.js
@@ -35,10 +35,13 @@ window.onload = function () {
     }
 
     cartBody.innerHTML = "";
+    let total =0;
 
     for (let i = 0; i < cart.length; i++) {
+        total += Number(cart[i].price);
 
         cartBody.innerHTML += `
+
         <tr class="shishir">
             <td>
                 <a href="#" onclick="removeItem(${i}); return false;">
@@ -55,12 +58,29 @@ window.onload = function () {
             <td>$${cart[i].price}</td>
 
             <td>
-                <input type="number" value="1" min="1">
+                <input type="number" value="1" min="1" onchange="updatePrice(${i}, this.value)">
             </td>
 
-            <td>$${cart[i].price}</td>
+             <td id="itemTotal${i}"> $${cart[i].price} </td>
         </tr>
         `;
     }
+    document.getElementById("subtotal").innerHTML = "$" + total;
+    document.getElementById("total").innerHTML = "<strong>$" + total + "</strong>";
 
 };
+
+function updatePrice(index,quantity){
+    let cart=JSON.parse(localStorage.getItem("cart")) || [];
+    quantity = Number(quantity);
+    let itemTotal = Number(cart[index].price) * quantity;
+    document.getElementById("itemTotal" + index).innerHTML = "$" + itemTotal;
+    let grandTotal = 0;
+    let qty=document.querySelectorAll("#cart-body input");
+    for(let i = 0; i< cart.length; i++) {
+        grandTotal += Number(cart[i].price) * Number(qty[i].value);
+    }
+    document.getElementById("subtotal").innerHTML = "$" + grandTotal;
+    document.getElementById("total").innerHTML = "<strong>$" + grandTotal + "</strong>";
+
+}
